8bab786687074cb16683403632dce10eadc5e7c1
bankart <bankart0218@gmail.com>
2018-04-03 17:10:35 +0900

    간단한 카드 매칭 게임

--- CardMatching/Concentration.swift
+++ CardMatching/Concentration.swift
@@ -25,16 +25,31 @@ struct Card {
 }
 
 class Concentration {
-    private var touchedCardIndex: Int?
+    private var touchedCardIndex: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+                print(cards[index])
+            }
+        }
+    }
     private(set) var cards = [Card]()
     private var numberOfPairsOfCards: Int = -1
     init(numberOfPairsOfCards: Int) {
-//        for _ in 0..<numberOfPairsOfCards {
-//            let card = Card()
-//            cards += [card, card]
-//        }
-//        // TODO: shuffle the cards
-//        cards = shuffle(cards)
+        assert(numberOfPairsOfCards > 0, "Concentration.init(numberOfPairsOfCards: \(numberOfPairsOfCards)): you must have at least one pair of card")
         self.numberOfPairsOfCards = numberOfPairsOfCards
         reset()
     }
@@ -67,24 +82,18 @@ class Concentration {
     }
     
     func chooseCard(at index: Int) {
-        if touchedCardIndex == nil {
-            for cardIndex in cards.indices {
-                if !cards[cardIndex].isMatched {
-                    cards[cardIndex].isFaceUp = false
-                }
-            }
-            cards[index].isFaceUp = true
-            touchedCardIndex = index
-        } else {
-            if touchedCardIndex! != index {
-                if cards[touchedCardIndex!].identifier == cards[index].identifier {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): chosen index not int the cards")
+        if !cards[index].isMatched {
+            if let matchIndex = touchedCardIndex, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
                     cards[index].isMatched = true
-                    cards[touchedCardIndex!].isMatched = true
-
+                    print("matched!!!")
                 }
-                touchedCardIndex = nil
+                cards[index].isFaceUp = true
+            } else {
+                touchedCardIndex = index
             }
-            cards[index].isFaceUp = true
         }
     }
 }
--- CardMatching/ViewController.swift
+++ CardMatching/ViewController.swift
@@ -5,80 +5,94 @@
 //  Created by taehoon lee on 2018. 4. 2..
 //  Copyright © 2018년 taehoon lee. All rights reserved.
 //
+//  외부에서 읽고/쓰고/호출해도 괜찮으면 internal
+//  외부에서 읽기만 하는건 괜찮으면 private(set)
+//  내부에서만 사용해야하면 private, 추후에 상황에 따라 접근 제한자는 변경할 수 있으므로 미심적을 때에는 private
+//
 
 import UIKit
 
 class ViewController: UIViewController {
-    private var animationActivated: Bool = false
-    private let targetBgColor: UIColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
-    private var flipCount: Int = 0 {
+//    private var queue: DispatchQueue?
+//    private var animationActivated: Bool = false
+//    private let targetBgColor: UIColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+    private var finished: Bool = false
+    
+    
+    private var emojies = ["🧙‍♂️", "🧛‍♂️", "🧟‍♀️", "👻", "😈", "🎃", "💀", "👽", "🧞‍♂️", "🧜‍♀️", "💃", "👯‍♂️", "🐒"]
+    private var emojiesForRandom: [String]?
+    private lazy var game: Concentration = {
+        // numberOfPairsOfCards 가 UI 와 강하게 결합한 프로퍼티이기 때문에 game 프로퍼티는 private 이 된다.
+        return Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
+    }()
+    
+    // 카드가 몇 개인지는 프로젝트 내부에서는 알아도 괜찮기 때문에 internal
+    var numberOfPairsOfCards: Int {
+        return (cardButtons.count + 1) / 2
+    }
+    
+    // ViewController 인스턴스에 영향을 주는 프로퍼티이므로 인스턴스 내부에서만 set 할 수 있어야 한다.
+    private(set) var flipCount: Int = 0 {
         didSet {
-            restartButton.setTitle("Restart!! (flipted: \(flipCount))", for: .normal)
-            print("flipCount: \(flipCount), warningCount: \(warningCount)")
-//            if flipCount >= warningCount, animationActivated == false {
-//                startAinmation()
-//            }
+            print("flipCount: \(flipCount), numberOfPairsOfCards: \(numberOfPairsOfCards)")
+            if flipCount >= numberOfPairsOfCards {
+                restartButton.setTitle("Restart!!", for: .normal)
+                restartButton.isUserInteractionEnabled = true
+                // delay 시간 동안 self 가 nil 될 수 있으므로 [weak self] 처리 해준다.
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) { [weak self] in
+                    self?.finished = true
+                    self?.reloadCards()
+                }
+            } else {
+                restartButton.setTitle("Card Matched: \(flipCount)", for: .normal)
+            }
         }
     }
     
-    private var emojies = ["🧙‍♂️", "🧛‍♂️", "🧟‍♀️", "👻", "😈", "🎃", "💀", "👽", "🧞‍♂️", "🧜‍♀️", "💃", "👯‍♂️", "🐒"]
+    @IBOutlet private weak var restartButton: UIButton!
+    @IBOutlet private var cardButtons: [UIButton]!
     
-    private lazy var game: Concentration = {
-        return Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
-    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        restartGame()
+    }
     
-    @IBOutlet weak var restartButton: UIButton!
-    @IBOutlet var cardButtons: [UIButton]!
-    private lazy var warningCount: Int = {
-        return cardButtons.count / 2
-    }()
-    
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction private func touchCard(_ sender: UIButton) {
         guard let emojiIndex = cardButtons.index(of: sender) else {
             print("Oops")
             return
         }
         
-        flipCount += 1
         game.chooseCard(at: emojiIndex)
+        if game.cards[emojiIndex].isMatched {
+            flipCount += 1
+        }
         reloadCards()
     }
     
-    @IBAction func restartGame() {
-        print("let's play more!!!")
-//        stopAnimation()
+    @IBAction private func restartGame() {
+        print("let's play!!!")
         flipCount = 0
+        finished = false
+        emojiesForRandom = emojies
         game.reset()
         reloadCards()
+        restartButton.isUserInteractionEnabled = false
     }
-    
-//    private func startAinmation() {
-//        print("\(#function)")
-//        animationActivated = true
-//
-//        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
-//            if self.view.backgroundColor == self.targetBgColor {
-//                self.view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-//            } else {
-//                self.view.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
-//            }
-//        }
-//    }
-//
-//    private func stopAnimation() {
-//        print("\(#function)")
-//        animationActivated = false
-//        DispatchQueue.main.suspend()
-//        self.view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-//    }
     
     private func reloadCards() {
         for index in cardButtons.indices {
             let btn = cardButtons[index]
             let card = game.cards[index]
-            flip(for: btn,
-                 with: card.isFaceUp ? emoji(for: card) : "",
-                 backgroundColor: card.isFaceUp ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1))
+            if finished {
+                flip(for: btn,
+                     with: "",
+                     backgroundColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+            } else {
+                flip(for: btn,
+                     with: card.isFaceUp ? emoji(for: card) : "",
+                     backgroundColor: card.isFaceUp ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : (card.isMatched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) : #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)))
+            }
         }
     }
     
@@ -95,12 +109,25 @@ class ViewController: UIViewController {
     
     private var emojiDic = [Int: String]()
     private func emoji(for card: Card) -> String {
-        if emojiDic[card.identifier] == nil, emojies.count > 0 {
-            let randomIndex = Int(arc4random_uniform(UInt32(emojies.count)))
-            emojiDic[card.identifier] = emojies.remove(at: randomIndex)
+        if emojiDic[card.identifier] == nil, emojiesForRandom!.count > 0 {
+//            // 랜덤 이모지 뽑아내는 코드는 컨버팅이 많고 불편하다. 이런 부분은 extension 으로 뽑아낸다.
+//            let randomIndex = Int(arc4random_uniform(UInt32(emojiesForRandom!.count)))
+            emojiDic[card.identifier] = emojiesForRandom!.remove(at: emojiesForRandom!.count.arc4random)
         }
         return emojiDic[card.identifier] ?? "??"
     }
     
 }
 
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
